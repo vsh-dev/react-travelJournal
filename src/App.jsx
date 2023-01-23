edc9ea8e39487562059e3a8fa3a85547dd31d5f1
vsh-dev <v.shumeliak.edu@gmail.com>
2023-01-23 13:30:14 +0200

    refactor: add import and etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
-  return <Header />;
+  const cards = data.map(function (item) {
+    return <Card {...item} />;
+  });
+  return (
+    <>
+      <Header />
+      <section className="card">{cards}</section>
+    </>
+  );
 }
 export default App;
