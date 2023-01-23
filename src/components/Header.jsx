7bb59d6cf48a90c6ee8c9610b6fb02639c99f5f8
vsh-dev <v.shumeliak.edu@gmail.com>
2023-01-23 12:40:00 +0200

    feat: add Header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import logo from "../assets/logo.svg";
 
 export default function Header() {
-  return <></>;
+  return (
+    <header className="header">
+      <img
+        className="header--logo"
+        src={logo}
+        alt="earth logo"
+        width={24}
+        height={24}
+      />
+      <h2 className="header--title">my travel journal.</h2>
+    </header>
+  );
 }
